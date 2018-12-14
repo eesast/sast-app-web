851678d1c082a559ea10da915ee26d5de85e39b5
Robert Ying <yingrui205@gmail.com>
2018-12-14 06:02:25 -0700

    Push docker also with latest tag

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t yingrui205/sast-app-web:$TRAVIS_TAG .
+docker build -t yingrui205/sast-app-web:$TRAVIS_TAG -t yingrui205/sast-app-web:latest .
 docker push yingrui205/sast-app-web:$TRAVIS_TAG
+docker push yingrui205/sast-app-web:latest
