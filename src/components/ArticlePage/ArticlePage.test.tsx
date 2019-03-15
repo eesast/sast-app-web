41d9f496d6a9a215923947349f8df5b7b0aee9f3
Robert Ying <yingrui205@gmail.com>
2019-03-16 02:59:50 +0800

    Fix access undefined for `role`

--- src/components/ArticlePage/ArticlePage.test.tsx
+++ src/components/ArticlePage/ArticlePage.test.tsx
@@ -1,12 +1,15 @@
 import { mount } from "enzyme";
 import React from "react";
 import { MemoryRouter, Route } from "react-router-dom";
+import { AuthProvider } from "../AuthContext/AuthContext";
 import ArticlePage from "./ArticlePage";
 
 it("renders without crashing", () => {
   mount(
-    <MemoryRouter initialEntries={["/articles/tensorflow-first-look"]}>
-      <Route path="/articles/:alias" component={ArticlePage} />
-    </MemoryRouter>
+    <AuthProvider>
+      <MemoryRouter initialEntries={["/articles/tensorflow-first-look"]}>
+        <Route path="/articles/:alias" component={ArticlePage} />
+      </MemoryRouter>
+    </AuthProvider>
   );
 });
